a499973ad9f92e91d6274c4e2efe0681d3eeaec8
Johan Polo <johan.s310806@gmail.com>
2024-12-02 08:31:59 -0500

    Ejercicio 1

--- JS/ejercicio01.js
+++ JS/ejercicio01.js
@@ -2,4 +2,4 @@ let edad = parseInt(prompt("Ingreses su edad")); //Solicito la edad y la alamace
 
 if (edad <= 0) alert("El número ingresado debe ser positivo..."); //Si la edad es menor o igual a 0, muestre mensaje.
 if (edad < 18 && edad > 0) alert("Usted es menor de edad..."); //Si edad es menor a 18 y mayor a 0 entonces muestres que es menor de edad.
-if (edad >= 18) alert("Usted es mayor de edad...);  //Si edad es mayor o igual a 18 muestre que es mayor de edad.
+if (edad >= 18) alert("Usted es mayor de edad...");  //Si edad es mayor o igual a 18 muestre que es mayor de edad.
