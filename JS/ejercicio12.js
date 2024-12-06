4bb38a6f3b1b363fd49b6435b37a12ad0daa189e
Johan Polo <johan.s310806@gmail.com>
2024-12-06 17:47:28 -0500

    Ejercicio 12

---
+++ JS/ejercicio12.js
@@ -0,0 +1,19 @@
+
+// Solicito al usuario ingresar el largo y el ancho del rectangulo.
+let largo = parseFloat(prompt("Ingrese el largo del rectangulo en cm"));
+let ancho = parseFloat(prompt("Ingrese el ancho del rectangulo en cm"));
+
+// Aqui manejo exepciones para validar que las medidas no sean iguales porque en ese caso seria un cuadrado y que los valores ingresados sean números.
+try {
+    if (largo === ancho) throw new Error("Las medidas son iguales (CUADRADO)");
+    else if (isNaN(largo) || isNaN(ancho)) throw new Error("Alguno de los valores ingresados no es un número.");
+    else alert(`El area del rectangulo es: ${areaRectan(largo, ancho)} cm^2`);
+    
+} catch (error) {
+    alert(error)
+}
+// Creo la función donde se realiza la operación para calcular el area del rectangulo.
+function areaRectan(largo, ancho) {
+    let area = largo * ancho;
+    return area;
+}
