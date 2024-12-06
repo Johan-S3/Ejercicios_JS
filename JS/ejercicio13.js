a2ccdfc4a692f612a080da5a594367cb97c9af25
Johan Polo <johan.s310806@gmail.com>
2024-12-06 18:34:35 -0500

    Ejercicio 13

--- JS/ejercicio13.js
+++ JS/ejercicio13.js
@@ -1,8 +1,10 @@
+// Solicito ingresar 4 números
 let num1 = parseFloat(prompt("Ingrese el primer número"));
 let num2 = parseFloat(prompt("Ingrese el segundo número"));
 let num3 = parseFloat(prompt("Ingrese el tercer número"));
 let num4 = parseFloat(prompt("Ingrese el cuarto número"));
 
+// Manejo exepciones para validar que alguno de los valores no sea diferente a un número.
 try {
     if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4)) throw new Error("Uno de los datos ingresado no es un número.");
     else {
@@ -12,6 +14,7 @@ try {
     alert(error);
 }
 
+// Creo una función donde se realiza la operación para calcular la media de los 4 números ingresados.
 function calcularMedia(num1, num2, num3, num4) {
     let array = [num1, num2, num3, num4]
     let acumulador = 0;
