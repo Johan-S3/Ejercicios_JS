247214fa02cc20ad4f73441e4f9512071f1d517e
Johan Polo <johan.s310806@gmail.com>
2024-12-09 11:34:06 -0500

    Avance jercicio 28

---
+++ JS/ejercicio28.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Ingrese el nombre del empleado:");
+let horasTrab = parseInt(prompt(`¿cuantas horas trabajó ${nombre}?:`));
+let pagoHora = parseFloat(prompt("¿Que valor tiene la hora de trabajo?:"));
+
