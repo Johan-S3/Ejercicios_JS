2821b94359a3d3b2cbd1ab1941f36bfca50931ed
Johan Polo <johan.s310806@gmail.com>
2024-12-09 22:40:17 -0500

    Ejercicio 38

---
+++ JS/ejercicio38.js
@@ -0,0 +1,9 @@
+// Declaro e asigno en variables los valores que le corresponder a martin y jairo.
+parteMartin = 2 / 3; 
+parteJairo = 1 / 4;
+
+// Declaro e asigno la parte de lorena restando 1 que seria el total de todo, menos la parte de martin y jairo.
+parteLorena = 1 - (parteMartin + parteJairo);
+
+// Muestro la parte de lorena.
+alert(`A lorena le corresponde: ${parteLorena}`);
