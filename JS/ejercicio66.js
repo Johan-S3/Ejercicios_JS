92f26f12a76ed99e00fa024be6d1544c9a464bbe
Johan Polo <johan.s310806@gmail.com>
2024-12-12 14:16:03 -0500

    Ejercicio 66

--- JS/ejercicio66.js
+++ JS/ejercicio66.js
@@ -2,18 +2,21 @@
 let capital = parseFloat(prompt("Ingrese la cantidad de dinero a invertir: "));
 let interes  = parseInt(prompt("¿De cuanto es el interes anual que se obtendrá?"));
 let anios = parseInt(prompt("¿A cuantos años será la inversión?"));
+if (isNaN(capital) || isNaN(interes) || isNaN(anios)) {
+    alert(`Algún valor ingresado no es valido`);
+}else{
+    // Declaro e inicilizo varaible en 0 donde se asignara el capital sumando su interes anual.
+    let total = 0;
 
-// Declaro e inicilizo varaible en 0 donde se asignara el capital sumando su interes anual.
-let total = 0;
+    // Creo bucle para iterar la cantidad de años ingresados y mostrar el total de capital anual dependiendo de la inversion y del interes anual ingresado.
+    for (let i = 1; i <= anios; i++) {
+        alert(`En el año ${i} el capital será de: \n$${calCapital(capital, (interes / 100))}`);
+        capital = calCapital(capital, (interes / 100));
+    }
 
-// Creo bucle para iterar la cantidad de años ingresados y mostrar el total de capital anual dependiendo de la inversion y del interes anual ingresado.
-for (let i = 1; i <= anios; i++) {
-    alert(`En el año ${i} el capital será de: \n$${calCapital(capital, (interes / 100))}`);
-    capital = calCapital(capital, (interes / 100));
-}
-
-// Creo función donde se calcula el capital recibiendo como parametros el capital ingresado y el interes anual.
-function calCapital(capital, interes) {
-    total = capital + (capital * interes);
-    return total;
+    // Creo función donde se calcula el capital recibiendo como parametros el capital ingresado y el interes anual.
+    function calCapital(capital, interes) {
+        total = capital + (capital * interes);
+        return total;
+    }
 }
