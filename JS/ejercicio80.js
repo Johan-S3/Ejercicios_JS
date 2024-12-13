9d0521facdd63006807b36b00c002cbe1f03ce4d
Johan Polo <johan.s310806@gmail.com>
2024-12-12 22:13:01 -0500

    Ejercicio 78

---
+++ JS/ejercicio80.js
@@ -0,0 +1,7 @@
+// Creo un bucle for
+for (let i = 1; i <= 10; i++) {
+    // Si el número de iteracion es diferente a 5 mostrar una liena de tabla multiplicacion normal
+    if (i != 5) console.log(`5 X ${i} = ${5*i}`);
+    // Si no es diferente a 5 entonces continuar con el programa.
+    else continue;
+}
