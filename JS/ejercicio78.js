21917224c4a950a190311db38b0df619c87194eb
Johan Polo <johan.s310806@gmail.com>
2024-12-12 21:56:46 -0500

    Ejercicio 78

---
+++ JS/ejercicio78.js
@@ -0,0 +1,12 @@
+// Declaro variable e asigno el valor ingresado por el usuario.
+let palabra = prompt("Ingrese una palabra:");
+
+// Creo funcion donde se calcula longitud de la variable.
+function longitud(palabra) {
+    if(palabra.length < 5) return "Palabra corta";
+    else return "Palabra larga";
+}
+
+// Valido si el valor ingresdao es una cadena de texto vacia o es nulo
+if (palabra == null || palabra === "") alert("Valor ingresado no valido.");
+else alert(`La palabra ingresa es una: \n${longitud(palabra)}`);
