9ba23747f640e20dbf5232aec87dd6c27d209dcd
Johan Polo <johan.s310806@gmail.com>
2024-12-12 20:14:39 -0500

    Ejercicio 73

--- JS/ejercicio73.js
+++ JS/ejercicio73.js
@@ -3,12 +3,14 @@ let frase = prompt("Ingrese una frase: ");
 let letra = prompt("Ingrese una letra para buscar en la frase:");
 
 // Valido que los valores de los satos ingresados no sea un cadena vacia o que la logitud de la variable letra sea diferente a 1.
-if (frase === "" || letra === "" || letra.length != 1) alert("Error en el ingreso de datos.");
+if (frase === "" || letra === "" || frase === null || letra === null) alert("Error en el ingreso de datos.");
 else {
-    // Si no es asi creo una expresion regular...
-    let regEx = new RegExp(letra, "gi"); //Utilizo banderas g y i (busqueda global) y (omisión de mayusculas y minusculas)
-    let coincidencias = frase.match(regEx); //Declaro una varaible donde se almacenera las coincidencias encontradas que en este caso será en un array ya que usamos el metodo match-.
+    if (letra.length != 1) {
+        // Si no es asi creo una expresion regular...
+        let regEx = new RegExp(letra, "gi"); //Utilizo banderas g y i (busqueda global) y (omisión de mayusculas y minusculas)
+        let coincidencias = frase.match(regEx); //Declaro una varaible donde se almacenera las coincidencias encontradas que en este caso será en un array ya que usamos el metodo match-.
 
-    // Por ultimo, muestro la cantidad de coincidencias.
-    alert(`La letra ${letra} está en la frase: \n${coincidencias.length} veces.`);
+        // Por ultimo, muestro la cantidad de coincidencias.
+        alert(`La letra ${letra} está en la frase: \n${coincidencias.length} veces.`);
+    }
 }
