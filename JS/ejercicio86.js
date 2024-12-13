ed78dc8e0d9cf50264ee64bfb43942594e4439c1
Johan Polo <johan.s310806@gmail.com>
2024-12-12 23:07:30 -0500

    Ejercicio 85+6

---
+++ JS/ejercicio86.js
@@ -0,0 +1,14 @@
+// Solicitar al usuario que ingrese los dos números
+let numero1 = parseInt(prompt("Ingrese el primer número:"));
+let numero2 = parseInt(prompt("Ingrese el segundo número:"));
+
+
+function sumar(num1, num2) {
+    // Sumar los dos números
+    let suma = num1 + num2;
+    return suma.toString(2); // ToStrign(2) lo que hace es convertir la suma a binario y devolverlo como cadena
+}
+
+
+// Calcular la suma en binario y mostrar el resultado
+alert(`La suma de ${numero1} y ${numero2} en binario es: ${sumar(numero1, numero2)}`);
